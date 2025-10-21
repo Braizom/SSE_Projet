bcbfc523ed1404d24fd18b5e108c877b24e7214c
Braizom <ambroise.boisseaux@hotmail.com>
2025-10-21 15:59:33 +0200

    First try menu

--- SSE_Int.ps1
+++ SSE_Int.ps1
@@ -1,3 +1,33 @@
-[void] [System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms')
-$FolderBrowserDialog = New-Object System.Windows.Forms.FolderBrowserDialog
-$FolderBrowserDialog.ShowDialog()
+Function Get-Folder(){
+    [void] [System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms')
+    $FolderBrowserDialog = New-Object System.Windows.Forms.FolderBrowserDialog
+    [void] $FolderBrowserDialog.ShowDialog()
+    return $FolderBrowserDialog.SelectedPath
+}
+
+Function Get-File(){
+    [void] [System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms')
+    $OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
+    [void] $OpenFileDialog.ShowDialog()
+    return $OpenFileDialog.FileName
+}
+
+
+Function Show-Menu(){
+    $menuOptions = @(
+        '1: Select Folder'
+        '2: Select File'
+    )
+
+    foreach ($option in $menuOptions) {
+        Write-Host $option
+    }
+
+    switch (Read-Host 'Please select an option (1-2)') {
+        '1' { return Get-Folder }
+        '2' { return Get-File }
+        default { Write-Host 'Invalid choice. Please try again.'; return Show-Menu }
+    }
+}
+
+Show-Menu
